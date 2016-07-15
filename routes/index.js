334c6bcac1a04c73c3916a7c0f78b70fec09f8ed
JoeyHicklin <j.hicklin87@yahoo.com>
2016-07-15 14:32:29 -0700

    Single SQL statement working on single page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var app = require('../app');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'NullSpeak',
+    mainTopic: mainTopic
+  });
 });
+var sql = require('mysql');
+var connection = sql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'discussion'
+});
+
+
+connection.connect();
+
+connection.query("SELECT TOPIC FROM main_topic WHERE QUEUE_NUM='0'", function(err, rows, fields){
+  //if(err);
+
+  mainTopic = rows[0].TOPIC;
+});
+connection.end();
+
 
 module.exports = router;
